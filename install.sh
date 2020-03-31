6d042efbd5e368b4b57a1c43e193d8b39a418495
Dave Horton <dhorton668@outlook.com>
2020-03-30 22:25:27 -0500

    Improvements to install process.

--- install.sh
+++ install.sh
@@ -4,12 +4,11 @@ echo -n "Checking root privileges. "
 if [ "$(id -u)" == "0" ]; then
   echo "OK."
 else
-  echo "Sorry, Charlie.  You must be root to run the install script."
+  echo "Sorry, Charlie. You must be root to run the install script."
   exit 1
 fi
 
 # Configure Time Sync
-echo "Configuring Time Sync..."
 sysrc ntpd_enable="YES"
 sysrc ntpd_sync_on_start="YES"
 ln -s /usr/share/zoneinfo/Etc/GMT /etc/localtime
@@ -32,18 +31,20 @@ echo "Configure Samba..."
 # Copy boilerplate Samba config file.
 cp etc/smb4.conf /usr/local/etc/
 
-# Enable and start service.
+# Enable and start services.
 sysrc samba_server_enable="YES"
 service samba_server start
 
 echo "Installing WeeNAS API service..."
-PWD=$(pwd)
 install -o0 -g0 -m755 etc/rc.d/weenas_api /usr/local/etc/rc.d
 sysrc weenas_api_enable="YES"
-sysrc weenas_api_home="$PWD"
+sysrc weenas_api_home="$(pwd)"
 sysrc weenas_api_port="9000"
 service weenas_api start
 
-# Finish
+# Append home filesystem to /etc/fstab for later.
+echo "/dev/ufs/homefs   /home   ufs   rw,noatime   1   2" >> /etc/fstab
+
+# Finish.
 IP="$(ifconfig ue0 | awk '/inet/ { print $2 }')"
 echo "Open a web browser to http://${IP}:9000 to configure the system."
