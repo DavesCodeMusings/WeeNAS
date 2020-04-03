05a204bcda61d0304884100d9f51676a991c8926
Dave Horton <dhorton668@outlook.com>
2020-04-03 00:40:36 -0500

    Moved homefs creation to install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,41 @@ fi
 # Set shell scripts to executable. (.zip files don't store the x perms.)
 find . -name '*.sh' | xargs chmod +x
 
+# Discover USB flash drive.
+DEVICE=da0
+geom disk status $DEVICE >/dev/null 2>&1
+if [ "$?" != "0" ]; then
+  echo "No USB mass storage device detected. Exiting installation."
+  exit 2
+fi
+
+# Confirm formatting for FreeBSD.
+echo
+echo "The following USB mass storage device was detected:"
+geom disk list $DEVICE | egrep 'Name|Mediasize|descr' | sed 's/^[0-9]\./  /'
+if [ "$(gpart show $DEVICE | grep freebsd)" != "" ]; then
+  echo
+  echo "   There is an existing FreeBSD partition on this device."
+fi
+echo
+echo "Do you wish to format this device and use it for WeeNAS home drives?"
+echo -n "ALL DATA CURRENTLY ON THE DEVICE WILL BE LOST [y/N]? "
+read OVERWRITE
+if [ "$OVERWRITE" == "y" ] || [ "$OVERWRITE" == "Y" ]; then
+  echo "Preparing $DEVICE for WeeNAS home drives."
+  gpart destroy -F $DEVICE
+  gpart create -s GPT $DEVICE
+  gpart add -t freebsd-ufs $DEVICE
+  newfs -j -L homefs /dev/${DEVICE}p1
+else
+  echo -n "Continue installation without formatting device [y/N]?"
+  read CONTINUE
+  if [ "$CONTINUE" != "y" ] && [ "$CONTINUE" != "Y" ]; then
+    echo "Exiting installation."
+    exit 3
+  fi
+fi
+
 # Configure time sync.
 sysrc ntpd_enable="YES"
 sysrc ntpd_sync_on_start="YES"
@@ -61,4 +96,4 @@ fi
 
 # Finish.
 IP="$(ifconfig ue0 | awk '/inet/ { print $2 }')"
-echo "Open a web browser to http://${IP}:9000 to configure the system."
+echo "Open a web browser to http://${IP}:9000 to finish configuring the system."
