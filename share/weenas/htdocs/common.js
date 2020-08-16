b93b1c503547ad4852d900e9d2314ab91c55c2d4
Dave Horton <dhorton668@outlook.com>
2020-08-16 13:58:29 -0500

    Corrected authToken name

--- share/weenas/htdocs/common.js
+++ share/weenas/htdocs/common.js
@@ -5,7 +5,7 @@
 // Make an async call to the API url and pass the parsed JSON to the callback. 
 function AJAX(method, url, callback) {
   document.body.style.cursor = 'wait';
-  let authHeader = sessionStorage.getItem('auth');
+  let authHeader = sessionStorage.getItem('authToken');
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState === 4) {
