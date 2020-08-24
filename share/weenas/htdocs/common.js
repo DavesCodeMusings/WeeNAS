9bb07dcde826014f1dbe33ee1a22d14130a7831e
Dave Horton <dhorton668@outlook.com>
2020-08-23 21:41:36 -0500

    Rename AJAX to apiGet; add apiPost

--- share/weenas/htdocs/common.js
+++ share/weenas/htdocs/common.js
@@ -3,7 +3,7 @@
 */
 
 // Make an async call to the API url and pass the parsed JSON to the callback. 
-function AJAX(method, url, callback) {
+function apiGet(url, callback) {
   document.body.style.cursor = 'wait';
   let authHeader = sessionStorage.getItem('authToken');
   let xhttp = new XMLHttpRequest();
@@ -18,7 +18,29 @@ function AJAX(method, url, callback) {
       document.body.style.cursor = 'default';
     }
   };
-  xhttp.open(method, url, true);
+  xhttp.open('GET', url, true);
   xhttp.setRequestHeader('Authorization', authHeader);
   xhttp.send();
+}
+
+// Make an async call to the API url and pass the parsed JSON to the callback. 
+function apiPost(url, body, callback) {
+  document.body.style.cursor = 'wait';
+  let authHeader = sessionStorage.getItem('authToken');
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        callback(JSON.parse(this.responseText));
+      }
+      else {
+        alert(`Error communicating with server.\n${this.responseText}`);
+      }
+      document.body.style.cursor = 'default';
+    }
+  };
+  xhttp.open('POST', url, true);
+  xhttp.setRequestHeader('Authorization', authHeader);
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(body);
 }
