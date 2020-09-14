5e96de00b03bdf4c07357f233b8ad339a3ff1ae9
Dave Horton <dhorton668@outlook.com>
2020-09-13 20:21:47 -0500

    Add wnpasswd entry when creating

--- createusers.sh
+++ createusers.sh
@@ -3,11 +3,13 @@
 # Command-line user creation for WeeNAS
 #
 # Check root privileges.
-echo "Checking root privilges." >>$LOGFILE
-[ "$(id -u)" == "0" ] || { echo "Sorry, Charlie. You must be logged in as root to run the install script."; exit 1; }
+[ "$(id -u)" == "0" ] || { echo "Sorry, Charlie. You must be logged in as root to create accounts."; exit 1; }
 
-# Temporary file with access restricted to the root user.
-umask 0077
+# Start a new error log.
+ERROR_LOG="createusers.err"
+echo -n "" >$ERROR_LOG
+
+# Temporary filename with access restricted to the root user.
 USERLIST=$(mktemp)
 
 # Get the list of user accounts.
@@ -26,15 +28,19 @@ if dialog --no-lines --title "Create Users" --defaultno --yesno "Give user(s) ad
 
 # One more chance to cancel, then loop through list of accounts.
 if dialog --no-lines --title "Create Users" --defaultno --yesno "Last chance to change you mind. Create accounts?" 5 60; then
-  echo "" >cu-error.log
   for LOGIN in $LIST; do
-    dialog --no-lines --title "Create Users" --infobox "Creating account ${LOGIN}..." 4 60
-    pw group add $LOGIN
-    PASSWORD=$(pw user add $LOGIN -g $LOGIN -d /home/$LOGIN -m -s $SHELL -w random) 2>>cu-error.log
-    if [ $ADMIN -eq 1 ]; then pw groupmod -g 0 -m $LOGIN; fi
-    printf "%s\n%s\n" $PASSWORD $PASSWORD | smbpasswd -a $LOGIN >/dev/null 2>>cu-error.log
+    if ! grep ^$LOGIN: /etc/passwd >/dev/null 2>&1; then
+      dialog --no-lines --title "Create Users" --infobox "Creating account ${LOGIN}..." 4 60
+      pw group add $LOGIN
+      PASSWORD=$(pw user add $LOGIN -g $LOGIN -c '&' -m -s $SHELL -w random) 2>>$ERROR_LOG
+      if [ $ADMIN -eq 1 ]; then pw groupmod -g 0 -m $LOGIN; fi
+      printf "%s\n%s\n" $PASSWORD $PASSWORD | smbpasswd -a $LOGIN >/dev/null 2>>$ERROR_LOG
+      printf "%s:disabled\n" $LOGIN >> /usr/local/etc/weenas/wnpasswd 2>>$ERROR_LOG
+    else
+      echo "Skipping $LOGIN. Account already exists." >>$ERROR_LOG
+    fi
   done
-  cat cu-error.log
+  cat $ERROR_LOG
 else
   echo "Canceled. No users were created."
 fi
